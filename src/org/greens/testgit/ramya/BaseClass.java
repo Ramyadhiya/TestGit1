734b328b5fe842003c271e4a569a64025e017c3e
Ramyadhiya <ramya14.email@gmail.com>
2018-09-25 18:28:35 +0530

    Base class update

--- src/org/greens/testgit/ramya/BaseClass.java
+++ src/org/greens/testgit/ramya/BaseClass.java
@@ -1,8 +1,11 @@
 package org.greens.testgit.ramya;
 
 public class BaseClass {
+	public void welcome() {
+		System.out.println("Welcome to my project");
+	}
 	public static void main(String[] args) {
-		System.out.println("The first project using git tool");
+		System.out.println("My first Git project tool");
 	}
 
 }
